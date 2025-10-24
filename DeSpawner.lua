5c17abe5c36eb755a10d7f1d03bcb954e06c75e2
Anctarus7105 <idzhabkin@gmail.com>
2025-10-24 14:12:44 +0400

    DeSpawner.lua

---
+++ DeSpawner.lua
@@ -0,0 +1,31 @@
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://u sound id here"
+	cue2.Volume = 1
+	cue2.PlaybackSpeed = 0.7(u PBSpeed here)
+	cue2:Play()
+wait(1.01)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://u sound id here"
+	cue2.Volume = 1
+	cue2.PlaybackSpeed = 0.6(u PBSpeed here)
+	cue2:Play()
+wait(1.01)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://u sound id here"
+	cue2.Volume = 1
+	cue2.PlaybackSpeed = 0.5(u PBSpeed here)
+	cue2:Play()
+wait(1.01)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://u sound id here"
+	cue2.Volume = 1
+	cue2.PlaybackSpeed = 0.4(u PBSpeed here)
+	cue2:Play()
